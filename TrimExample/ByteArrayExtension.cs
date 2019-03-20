b7518978e8e738b185d03b07849056e4b8510b02
samfromdeath <samuel@intelogy.com.au>
2019-03-20 18:06:01 +1100

    Add project files.

---
+++ TrimExample/ByteArrayExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrimExample
+{
+    public static class ByteArrayExtension
+    {
+        public static byte[] Trim(this byte[] data)
+        {
+            if (data == null || data.Length == 0)
+                return data;
+
+            var list = new List<byte>(data);
+            bool removedFirst = false;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if(!char.IsWhiteSpace((char)list[i]))
+                {
+                    list.RemoveRange(0, i);
+                    removedFirst = true;
+                    break;
+                }
+            }
+            if (!removedFirst)
+                return new byte[] { };
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                if (!char.IsWhiteSpace((char)list[i]))
+                {
+                    list.RemoveRange(i + 1, list.Count - 1 - i);
+
+                    break;
+                }
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ TrimExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrimExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = " \t Hello World \t   ";
+
+            var bytes = Encoding.Default.GetBytes(str);
+
+            var trimmedBytes = bytes.Trim();
+
+            Console.WriteLine(Encoding.Default.GetString(trimmedBytes));
+        }
+    }
+}
